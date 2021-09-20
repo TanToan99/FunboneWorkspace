101c1fdc5f02c4ff8708188f6ae6e313fccb02ec
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-09-20 15:34:58 +0700

    fix(WorkspaceList component): Layout

--- src/components/common/ListWorkspace/index.jsx
+++ src/components/common/ListWorkspace/index.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Box, ListItemButton, ListItemText, ListItemIcon } from "@mui/material";
+import {
+  Box,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+  Container,
+  Grid,
+} from "@mui/material";
 import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import { useStyle } from "./style";
 import Public from "@mui/icons-material/Public";
@@ -13,53 +20,55 @@ const ListWorkspace = () => {
   const [open, setOpen] = useState(false);
   const classes = useStyle();
   return (
-    <div className={classes.wsContainer}>
-      <Box className={classes.listWorkspaces}>
-        <ListItemButton
-          alignItems="flex-start"
-          onClick={() => setOpen(!open)}
-          sx={{
-            "&:hover, &:focus": { "& svg": { opacity: open ? 0: 1 } },
-          }}
-        >
-          <ListItemText
-            primary="Workspaces"
-            primaryTypographyProps={{
-              fontSize: 25,
-              fontWeight: "medium",
-              lineHeight: "30px",
-              color: "#fff",
-            }}
-          />
-          <KeyboardArrowDown
-            sx={{
-              mr: -1,
-              opacity: 0,
-              color: "#fff",
-              transform: open ? "rotate(-180deg)" : "rotate(0)",
-              transition: "0.2s",
-            }}
-          />
-        </ListItemButton>
-        {open &&
-          data.map((item) => (
+    <Container maxWidth="xl" className={classes.wsContainer}>
+      <Grid container spacing={2}>
+        <Grid item xs={5}>
+          <Box className={classes.listWorkspaces}>
             <ListItemButton
-              key={item.label}
-              sx={{ minHeight: 35, color: "#fff" }}
+              alignItems="flex-start"
+              onClick={() => setOpen(!open)}
+              sx={{
+                "&:hover, &:focus": { "& svg": { opacity: open ? 0 : 1 } },
+              }}
             >
-              <ListItemIcon sx={{ color: "inherit" }}>{item.icon}</ListItemIcon>
               <ListItemText
-                primary={item.label}
-                primaryTypographyProps={{ fontSize: 15 }}
+                primary="Workspaces"
+                primaryTypographyProps={{
+                  fontSize: 25,
+                  fontWeight: "medium",
+                  lineHeight: "30px",
+                  color: "#fff",
+                }}
+              />
+              <KeyboardArrowDown
+                sx={{
+                  mr: -1,
+                  opacity: 0,
+                  color: "#fff",
+                  transform: open ? "rotate(-180deg)" : "rotate(0)",
+                  transition: "0.2s",
+                }}
               />
             </ListItemButton>
-          ))}
-      </Box>
-
-      <Box>
-        
-      </Box>
-    </div>
+            {open &&
+              data.map((item) => (
+                <ListItemButton
+                  key={item.label}
+                  sx={{ minHeight: 35, color: "#fff" }}
+                >
+                  <ListItemIcon sx={{ color: "inherit" }}>
+                    {item.icon}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={item.label}
+                    primaryTypographyProps={{ fontSize: 15 }}
+                  />
+                </ListItemButton>
+              ))}
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
--- src/components/common/ListWorkspace/style.js
+++ src/components/common/ListWorkspace/style.js
@@ -1,17 +1,19 @@
 import { makeStyles } from "@mui/styles";
 
 export const useStyle = makeStyles({
+
+
   wsContainer: {
     display: "flex",
     flexDirection: "row",
     width: "100%",
-    marginTop: "100px",
-    marginLeft: "50px",
-    height: "100%"
+    height: "100%",
+    margin: "10px 0",
   },
+
   listWorkspaces: {
-    width: "25%",
     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    justifyItems: "center"
+    justifyItems: "center",
+    boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
   },
 });
