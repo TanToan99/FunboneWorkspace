710a8083aa7d056fde267b930b26df1f2cd5904a
moon2510 <nguyetminhthibui@gmail.com>
2021-09-23 04:32:54 +0700

    feat(unity):Added unity at gameframe

--- src/components/landing/GameFrame/styles.js
+++ src/components/landing/GameFrame/styles.js
@@ -5,7 +5,7 @@ export const useStyles = makeStyles({
     position: "relative",
     background: "black",
     height: "100%",
-    boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
+    boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
   },
   videoBlock: {
     display: "flex",
@@ -14,5 +14,6 @@ export const useStyles = makeStyles({
     position: "absolute",
     height: "150px",
     width: "100%",
+    zIndex: "1",
   },
 });
--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -2,3 +2,4 @@ export { default as Login } from "./Login";
 export { default as RegisterComponent } from "./Register";
 export { default as VideoCall } from "./VideoCall";
 export { default as GameFrame } from "./GameFrame";
+export { default as Demo} from './Unity';
